6d1b524c99050d2aa8f65541fdb18d294d47c972
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-04-11 15:47:17 +0100

    Add stanza ID spoofing protection, fixes #4

--- test/lib/mam.prefs.js
+++ test/lib/mam.prefs.js
@@ -16,6 +16,8 @@ describe('Mam', function() {
             socket: socket,
             client: xmpp,
             trackId: function(id, callback) {
+                if (typeof id !== 'object')
+                    throw new Error('Stanza ID spoofing protection not in place')
                 this.callback = callback
             },
             makeCallback: function(error, data) {
